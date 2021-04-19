aeacd3becd8a40e7670d0a9f9ccae1462113cc15
reut <reut.kurzweil@mail.huji.ac.il>
2021-04-19 19:02:18 +0300

    start the project

--- app/src/main/java/android/exercise/mini/calculator/app/SimpleCalculatorImpl.java
+++ app/src/main/java/android/exercise/mini/calculator/app/SimpleCalculatorImpl.java
@@ -1,36 +1,50 @@
 package android.exercise.mini.calculator.app;
 
 import java.io.Serializable;
+import java.util.Stack;
+
+import static java.lang.Character.isDigit;
 
 public class SimpleCalculatorImpl implements SimpleCalculator {
+
+  private Stack calc = new Stack();
 
   // todo: add fields as needed
 
   @Override
   public String output() {
     // todo: return output based on the current state
-    return "implement me please";
+    // String userInput = editTextTitle.getText().toString();
+    // textViewTitle.setText(userInput);
+    return "reut";
   }
 
   @Override
   public void insertDigit(int digit) {
-    // todo: insert a digit
+    if (isDigit(digit)) {
+      calc.push(digit);
+    }
+    else {
+      throw new IllegalArgumentException();
+    }
+
   }
 
   @Override
   public void insertPlus() {
-    // todo: insert a plus
+    calc.push('+');
   }
 
   @Override
   public void insertMinus() {
-    // todo: insert a minus
+    calc.push('-');
   }
 
   @Override
   public void insertEquals() {
     // todo: calculate the equation. after calling `insertEquals()`, the output should be the result
     //  e.g. given input "14+3", calling `insertEquals()`, and calling `output()`, output should be "17"
+    print output();
   }
 
   @Override
@@ -40,17 +54,23 @@ public class SimpleCalculatorImpl implements SimpleCalculator {
     //  if input was "12+3" and called `deleteLast()`, then delete the "3"
     //  if input was "12+" and called `deleteLast()`, then delete the "+"
     //  if no input was given, then there is nothing to do here
+    if (calc.empty()) {
+      return;
+    }
+    calc.pop();
   }
 
   @Override
   public void clear() {
     // todo: clear everything (same as no-input was never given)
+    calc.clear();
   }
 
   @Override
   public Serializable saveState() {
     CalculatorState state = new CalculatorState();
     // todo: insert all data to the state, so in the future we can load from this state
+    state = calc.clone();
     return state;
   }
 
